0b3938daf6c6e0a7c2958d52eb9a4b9e34465cd4
leitianjian <leitianjian@outlook.com>
2024-04-13 15:57:06 +0800

    merge rl functionality of three parts falling

--- demo/demo_simulation/three_part/simulation_with_action.cpp
+++ demo/demo_simulation/three_part/simulation_with_action.cpp
@@ -5,6 +5,8 @@
 #include "sire/middleware/sire_middleware.hpp"
 #include "sire/server/interface.hpp"
 
+#include <random>
+
 namespace sire::plan {
 struct GetParam {
   std::vector<std::vector<double>> part_pq;  // 怎么获得 body_pq?
@@ -21,7 +23,7 @@ struct GetParam {
   int currentPlanId;
 };
 struct SimulationWithAction::Imp {
-  sire::Size frame_skip_{25};
+  uint32_t frame_skip_{25};
 };
 auto SimulationWithAction::prepareNrt() -> void {
   option() |=
@@ -33,12 +35,19 @@ auto SimulationWithAction::prepareNrt() -> void {
   par.motors_a.resize(model()->motionPool().size(), 0);
   par.motors_v.resize(model()->motionPool().size());
   par.motors_p.resize(model()->motionPool().size());
-  par.motors_f.resize(model()->motionPool().size());
+  par.motors_f.resize(model()->motionPool().size()); // size = 2
   par.general_motion_p.resize(model()->generalMotionPool().size());
-  par.general_motion_v.resize(model()->generalMotionPool().size());
+  par.general_motion_v.resize(model()->generalMotionPool().size()); // size = 1
   auto& cs = *controlServer();
   auto& middleware = dynamic_cast<middleware::SireMiddleware&>(cs.middleWare());
-  auto& simulator = middleware.simulationLoop();
+  auto& simulator = middleware.simulatorBase();
+  uint32_t reset_flag = 0;
+  uint32_t delay = 0;
+  std::vector<double> position;
+  // random generator
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> uniform(0.9, 1.1);
   for (const auto& cmd_param : cmdParams()) {
     if (cmd_param.first == "action") {
       par.motors_f.clear();
@@ -50,35 +59,135 @@ auto SimulationWithAction::prepareNrt() -> void {
         std::copy(temp.begin(), temp.end(), par.motors_f.begin());
       else
         THROW_FILE_LINE("");
-
+   
     } else if (cmd_param.first == "frame_skip") {
       sire::Size temp = uint64Param(cmd_param.first);
       imp_->frame_skip_ = temp;
+
+    } else if (cmd_param.first == "reset") {
+      reset_flag = uint32Param(cmd_param.first);
+    
+    } else if (cmd_param.first == "delay") {
+      delay = std::min(uint32Param(cmd_param.first), imp_->frame_skip_);
+
+    } else if (cmd_param.first == "position") {
+      position.clear();
+      position.resize(
+          model()->motionPool().size() + model()->jointPool().size(), 0.0);
+      auto temp = matrixParam(cmd_param.first);
+      if (temp.size() == 1)
+        std::fill(position.begin(), position.end(), temp.toDouble());
+      else 
+        std::copy(temp.begin(), temp.end(), position.begin());
     }
   }
-  for (sire::Size i = 0; i < model()->motionPool().size(); ++i) {
-    dynamic_cast<aris::dynamic::SingleComponentForce&>(
-        model()->forcePool().at(i))
-        .setFce(par.motors_f[i]);
+  // reset
+  if (reset_flag > 0) {
+    auto xmlpath = std::filesystem::absolute(".");  // 获取当前工程所在的路径
+    const std::string xmlfile = "model.xml";
+    //auto& cs = aris::server::ControlServer::instance();
+    xmlpath = xmlpath / xmlfile;
+    aris::core::fromXmlFile(*model(), xmlpath);
+    model()->init();
+
+    // dynamics randomization
+    if (reset_flag == 2) {
+      for (std::size_t i = 1; i < model()->partPool().size(); ++i) {
+        const double* inertial = model()->partPool().at(i).prtIv();
+        // print
+        std::cout << "init: ";
+        for (int i = 0; i < 10; i++) {
+          std::cout << inertial[i] << " ";
+        }
+        std::cout << std::endl;
+        // random
+        const double rand_inertial[10] = {
+            uniform(gen) * inertial[0], 0, 0, 0, 0, 0,
+            uniform(gen) * inertial[6], 0, 0, 0};
+        model()->partPool().at(i).setPrtIv(rand_inertial);
+        model()->init();
+        // print
+        std::cout << "rand: ";
+        for (int i = 0; i < 10; i++) {
+          std::cout << inertial[i] << " ";
+        }
+        std::cout << std::endl << std::endl;
+      }
+    }
+
+    // 被动关节角度随机化
+    double joint_pos[6] = {0, 0, 0, 0, 0, position[0]};
+    model()->jointPool().at(0).makI()->setPe(
+        *model()->jointPool().at(0).makJ(), joint_pos, "123");
+    // 主动关节角度随机化
+    model()->motionPool().at(0).setMp(position[1]);
+    model()->motionPool().at(1).setMp(position[2]);
+    model()->forwardKinematics();
+    // imp_->frame_skip_ = 0;                          // reset时设置仿真0步
+    
+  } else {                                          // reset时不设置force
+    simulator.step(delay);  // 设置延迟
+    for (sire::Size i = 0; i < model()->motionPool().size(); ++i) {
+      dynamic_cast<aris::dynamic::SingleComponentForce&>(
+          model()->forcePool().at(i))
+          .setFce(par.motors_f[i]);
+    }
+    simulator.step(imp_->frame_skip_ - delay);
   }
-  simulator.step(imp_->frame_skip_);
 
   for (std::size_t i = 0; i < model()->generalMotionPool().size(); ++i) {
     auto& general_motion = model()->generalMotionPool().at(i);
-    // general_motion.updP();
-    std::array<double, 6> temp_p{0.0}, temp_v{0.0};
+    // update general_motion
+    general_motion.updP();  
+    // get general_motion_p
+    std::array<double, 6> temp_p{0.0};
     general_motion.getP(temp_p.data());
-    general_motion.getV(temp_v.data());
     par.general_motion_p[i].assign(temp_p.begin(), temp_p.end());
+    // get general_motion_v
+    std::array<double, 6> temp_v{0.0};
+    general_motion.getV(temp_v.data());
     par.general_motion_v[i].assign(temp_v.begin(), temp_v.end());
+    //aris::dynamic::dsp(1, 6, temp_v.data());
+  }
+  double joint0Pos[6]{0.0};
+  model()->jointPool().at(0).makI()->getPe(joint0Pos, "123");
+  double joint0Vel[6]{0.0};
+  model()->jointPool().at(0).makI()->getVa(joint0Vel);
+  // std::cout << joint1Pos << std::endl;
+  // std::cout << joint1Vel << std::endl;
+
+  for (std::size_t i = 0; i < model()->motionPool().size(); ++i) {
+    auto& motion = model()->motionPool().at(i);
+    // get motors_p
+    par.motors_p[i] = motion.mp();
+    // get motors_v
+    par.motors_v[i] = motion.mv();
+    // get motors_a
+    par.motors_a[i] = motion.ma();
   }
   auto& inter =
       dynamic_cast<server::ProgramWebInterface&>(cs.interfacePool().at(0));
   std::vector<std::pair<std::string, std::any>> out_param;
+  // push back general_motion_p
   out_param.push_back(std::make_pair<std::string, std::any>(
       "general_motion_p", nlohmann::json(par.general_motion_p)));
+  // push back general_motion_v
   out_param.push_back(std::make_pair<std::string, std::any>(
       "general_motion_v", nlohmann::json(par.general_motion_v)));
+  // push back joint 0
+  out_param.push_back(std::make_pair<std::string, std::any>(
+      "joint_p", nlohmann::json(joint0Pos)));
+  out_param.push_back(std::make_pair<std::string, std::any>(
+      "joint_v", nlohmann::json(joint0Vel)));
+  // push back motors_p
+  out_param.push_back(std::make_pair<std::string, std::any>(
+      "motors_p", nlohmann::json(par.motors_p)));
+  // push back motors_v
+  out_param.push_back(std::make_pair<std::string, std::any>(
+      "motors_v", nlohmann::json(par.motors_v)));
+  // push back motors_a
+  out_param.push_back(std::make_pair<std::string, std::any>(
+      "motors_a", nlohmann::json(par.motors_a)));
   ret() = out_param;
   return;
 }
@@ -90,6 +199,9 @@ SimulationWithAction::SimulationWithAction(const std::string& name)
       "	<GroupParam>"
       "  <Param name=\"action\" abbreviation=\"a\" default=\"{0,0}\"/>"
       "	 <Param name=\"frame_skip\" abbreviation=\"s\" default=\"25\"/>"
+      "	 <Param name=\"reset\" abbreviation=\"r\" default=\"0\"/>"
+      "	 <Param name=\"delay\" abbreviation=\"d\" default=\"0\"/>"
+      "  <Param name=\"position\" abbreviation=\"p\" default=\"{0,0}\"/>"
       "	</GroupParam>"
       "</Command>");
 }
